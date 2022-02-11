c61c8bd57b2c96ecc6c895bef89ffcff7da1f19f
kimtaeyoung0321 <kty986433@gmail.com>
2022-02-11 15:20:53 +0900

    css

--- js/weather.js
+++ js/weather.js
@@ -3,14 +3,16 @@ const API_KEY = "102d04450263df0660b12a55e6d46180";
 function onGeo(tomato) {
   const lat = tomato.coords.latitude;
   const lon = tomato.coords.longitude;
-  const url = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${API_KEY}`;
+  const url = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${API_KEY}&units=metric`;
   fetch(url)
     .then((response) => response.json())
     .then((data) => {
       const whereIAm = document.querySelector("#weather span:first-child");
-      const weather = document.querySelector("#weather span:last-child");
-      whereIAm.innerText = data.name;
-      weather.innerText = data.weather[0].main;
+      const weather = document.querySelector("#weather span:nth-child(2)");
+      const temp = document.querySelector("#weather span:last-child");
+      whereIAm.innerText = `지역: ${data.name}`;
+      weather.innerText = `날씨: ${data.weather[0].main}`;
+      temp.innerText = `기온: ${data.main.temp}`;
     });
 }
 function onGeoError() {}
